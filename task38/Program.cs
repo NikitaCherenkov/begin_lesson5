db943af11a4980c09886fbe280649f5e8868ca6f
Nikita <cherenkov204@gmail.com>
2022-03-27 18:27:50 +0300

    task38 -- fix

--- task38/Program.cs
+++ task38/Program.cs
@@ -2,17 +2,17 @@
 
 Random rnd = new Random();
 int count;
-double max = 0;
-double min = 0;
 Console.WriteLine("Введите количество элементов массива");
 string entry = Convert.ToString(Console.ReadLine());
 bool isNum = int.TryParse(entry, out count);
 if (isNum & count > 1) {
     double[] array = new double[count];
     for (int i = 0; i < count; i++) array[i] = rnd.Next() - rnd.NextDouble() * rnd.Next();
-    for (int n = 0; n < array.Length - 1; n++) {
-        max = Math.Max(array[n], array[n + 1]);
-        min = Math.Min(array[n], array[n + 1]);
+    double max = 0;
+    double min = array[0];
+    for (int n = 0; n < array.Length; n++) {
+        max = Math.Max(max, array[n]);
+        min = Math.Min(min, array[n]);
     }
     Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {max - min}");
 } else Console.WriteLine("Нужно ввести положительное целое число больше единицы");
