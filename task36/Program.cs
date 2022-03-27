45f743a698df0b628e356c4c4f8877bcca726bc3
Nikita <cherenkov204@gmail.com>
2022-03-27 15:01:35 +0300

    update task36

--- task36/Program.cs
+++ task36/Program.cs
@@ -9,8 +9,6 @@ bool isNum = int.TryParse(entry, out count);
 if (isNum & count > 0) {
     int[] array = new int[count];
     for (int i = 0; i < count; i++) array[i] = rnd.Next(100, 1000);
-    for (int n = 0; n < array.Length; n = n + 2) {
-        amount = amount + array[n];
-    }
+    for (int n = 0; n < array.Length; n = n + 2) amount = amount + array[n];
     Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {amount}");
 } else Console.WriteLine("Нужно ввести положительное целое число");
